205b34b7160e9647049a99236f1d9628b01507e5
abrhamisai17-cmd <abrhamisai17@gmail.com>
2025-11-19 08:54:27 -0600

    Actualización practica 19

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+
 const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) =>{
-    res.send('Hola NodeJS con Express')
-});
+app.set('view engine', 'ejs');
+
+const inicioRoute = require('./routes/inicioRoute');
+const productosRoute = require('./routes/productosRoute');
+const serviciosRoute = require('./routes/serviciosRoute');
+const contactoRoute = require('./routes/contactoRoute');
+
+app.set('view engine', 'ejs');
+
+app.use('/contacto', contactoRoute);
+app.use('/servicios', serviciosRoute);
+app.use('/', inicioRoute);
+app.use('/productos', productosRoute);
 
 app.listen(port, ()=>{
     console.log(`Servidor escuchando en http://localhost:${port}`);
